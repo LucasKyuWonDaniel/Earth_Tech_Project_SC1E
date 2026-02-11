302cc04a5b923b79562d7d1dcbdfa580a7d04869
lucas <lucas.lin@efrei.net>
2026-02-11 12:13:41 +0100

    2

--- Code/Menu/Menu.py
+++ Code/Menu/Menu.py
@@ -2,7 +2,7 @@ import pygame
 pygame.init()
 
 pygame.display.set_caption("Forest Frontiers")
-screen = pygame.display.set_mode((900,400))
+screen = pygame.display.set_mode((1530,795))
 running = True
 while running:
     for event in pygame.event.get():
