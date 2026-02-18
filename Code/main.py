0098b941a84434a79b54835adba0aa5c34bf504c
Victor_fqt <victor.fauquet@efrei.net>
2026-02-18 09:50:56 +0100

    Déplacements de base main.py

--- Code/main.py
+++ Code/main.py
@@ -1,5 +1,9 @@
 import pygame
-
+# Variables
+x = 0
+vx = 10
+y = 670
+vy = 10
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -10,7 +14,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, (255, 255, 255), (x,y,50,50))
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_q]:
+        x -= vx
+    if keys[pygame.K_d]:
+        x += vx
     pygame.display.flip()
     clock.tick(60)
 
