bae20a8592fa2e94dcff5aab39692c7dfaee89c8
Victor_fqt <victor.fauquet@efrei.net>
2026-03-04 14:44:49 +0100

    Déplacements de base main.py

--- Code/main.py
+++ Code/main.py
@@ -1,26 +1,79 @@
 import pygame
-# Variables
-x = 0
-vx = 10
-y = 670
-vy = 10
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
 
-while running:
+pygame.init()
+screen = pygame.display.set_mode((1240, 700))
+clock = pygame.time.Clock()
+
+joueur = pygame.Rect(100, 100, 40, 40)
+vy = 0
+gravite = 0.8
+vx = 0
+acceleration = 0.8
+friction = 0.7
+vitesse_max = 7
+
+plateformes = [
+    pygame.Rect(50, 500, 700, 30),  # Le sol
+    pygame.Rect(150, 400, 200, 20), # Plateforme 1
+    pygame.Rect(450, 300, 200, 20), # Plateforme 2
+]
+
+run = True
+while run:
+    screen.fill((30, 30, 30))
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, (255, 255, 255), (x,y,50,50))
+        if event.type == pygame.QUIT: run = False
+
+    # Mouvement horizontal
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_q]:
-        x -= vx
-    if keys[pygame.K_d]:
-        x += vx
+    direction = 0
+    if keys[pygame.K_LEFT] or keys[pygame.K_q]:
+        direction = -1
+    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        direction += 1
+
+    if direction != 0:
+        vx += direction * acceleration
+    else:
+        vx *= friction
+
+    if vx > vitesse_max:
+        vx = vitesse_max
+    if vx < -vitesse_max:
+        vx = -vitesse_max
+    if abs(vx) < 0.1:
+        vx = 0
+    joueur.x += vx
+
+    # Physique verticale
+    vy += gravite
+    joueur.y += vy
+
+    # Détection de collision
+    en_contact = False
+    for p in plateformes:
+        if joueur.colliderect(p):
+            if vy > 0 and not keys[pygame.K_DOWN]:
+                if (joueur.bottom - vy) <= p.top:
+                    joueur.bottom = p.top
+                    vy = 0
+                    en_contact = True
+
+
+            elif vy < 0:
+                if joueur.top >= p.bottom + vy - 1:
+                    joueur.top = p.bottom
+                    vy = 0
+
+    # Saut
+    if keys[pygame.K_SPACE] and en_contact:
+        vy = -16
+
+    # Dessin
+    pygame.draw.rect(screen, (0, 255, 100), joueur) # Joueur
+    for p in plateformes:
+        pygame.draw.rect(screen, (200, 200, 200), p) # Plateformes
+
     pygame.display.flip()
     clock.tick(60)
 
