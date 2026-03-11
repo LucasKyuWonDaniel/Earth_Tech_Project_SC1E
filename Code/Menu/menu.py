373db34f84b571c616566fde5d8401866a553cc1
nathan <nathan.fournier@efrei.net>
2026-03-11 21:26:33 +0100

    ajoute de commantaire

--- Code/Menu/menu.py
+++ Code/Menu/menu.py
@@ -3,13 +3,15 @@ from .niveau_menu import*
 from .main_menu import*
 from Code.Utils.Utils import draw_botton
 
-
+# fonction qui permet de faire tourner les menu
 def run_menu(screen, element, niveau, click, continue_click):
     screen.blit(element[0], (0, 0))
     screen.blit(element[1][0], element[1][1])
     niveau = draw_botton(screen, element[2:], click, niveau, continue_click)
     return niveau
 
+
+# fonction qui permet d'initialiser les menu
 def init_menu(menu, police):
     element = []
     if menu == -2:
